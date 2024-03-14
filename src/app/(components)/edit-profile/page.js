ebacbc439b8a8d6267b578ad192b70ebbe1383d9
Rohit Kewat <rohitwebd@gmail.com>
2024-03-14 19:47:14 +0530

    aaj hamare se kuch hua hi nhi

--- src/app/(components)/edit-profile/page.js
+++ src/app/(components)/edit-profile/page.js
@@ -2,46 +2,50 @@
 import React, { useState } from "react";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import profilepic from "../../../../public/images/defultprofile.png"
 import timelineimage from "../../../../public/images/timelinepic2.jpg"
+import profilepic from "../../../../public/images/defultprofile.png"
 import Image from "next/image";
 
 export default function Editprofile() {
-
-    const [profile, setProfile] = useState(profilepic);
-    function handleChange(e) {
-        console.log(e.target.files);
-        setProfile(URL.createObjectURL(e.target.files[0]));
-    }
 
     const [timelinepic, setTimeline] = useState(timelineimage);
     function timelineChange(e) {
         console.log(e.target.files);
         setTimeline(URL.createObjectURL(e.target.files[0]));
     }
+
+    const [profile, setProfile] = useState(profilepic);
+    function profileChange(f) {
+        console.log(f.target.files,'profile pic');
+        setProfile(URL.createObjectURL(f.target.files[0]));
+    }
+
     return (
         <>
             <div className="container-fluid profile-bg">
                 <div className="row ">
-                    <div className="col-md-12 p-0">
-                        <Image height={200} width={1400} src={timelinepic}/>
-                        <button className="align-items-center"><label htmlFor="file">Change cover</label></button>
+                    <div className="col-md-12 p-0 text-center">
+                        <Image height={200} width={1400} src={timelinepic} alt="timeline"/>
+                        <label htmlFor="file" className="btn btn-outline-dark btn-sm">Change cover</label>
                         <input type="file" id="file" onChange={timelineChange} className="d-none" />
-
                     </div>
                 </div>
+
                 <div className="row">
+
                     <div className="col-md-3 border-right">
                         <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-                            <Image className="rounded-circle mt-5" width={200} height={200} src={profile} />
-                            <label htmlFor="file"> <FontAwesomeIcon icon={faPlusCircle} /></label>
-                            <input type="file" id="file" onChange={handleChange} className="d-none" />
+                            <Image className="rounded-circle mt-5" width={200} height={200} src={profile} alt="profile" />
+                            <label htmlFor="profile"> <FontAwesomeIcon icon={faPlusCircle} /></label>
+                            <input type="file" id="profile" onChange={profileChange} className="d-none" />
+
 
                             <span className="font-weight-bold p-2">Rohit Kewat</span>
                             <span className="text-black-50">rohitwebd@gamil.com</span>
-                            <span> </span>
+
                         </div>
                     </div>
+
                     <div className="col-md-5 border-right">
                         <div className="p-3 py-5">
                             <div className="d-flex justify-content-between align-items-center mb-3">
@@ -50,25 +54,25 @@ export default function Editprofile() {
                             <div className="row mt-2">
                                 <div className="col-md-6">
                                     <label className="labels">Name</label>
-                                    <input type="text" className="form-control" placeholder="first name" value="" />
+                                    <input type="text" className="form-control" placeholder="first name" />
                                 </div>
                                 <div className="col-md-6">
                                     <label className="labels">Surname</label>
-                                    <input type="text" className="form-control" value="" placeholder="surname" />
+                                    <input type="text" className="form-control" placeholder="surname" />
                                 </div>
                             </div>
                             <div className="row mt-3">
                                 <div className="col-md-12 p-2">
                                     <label className="labels">About</label>
-                                    <input type="text" className="form-control" placeholder="about you" value="" />
+                                    <input type="text" className="form-control" placeholder="about you" />
                                 </div>
                                 <div className="col-md-12 p-2">
                                     <label className="labels">Dob</label>
-                                    <input type="text" className="form-control" placeholder="date of birth" value="" />
+                                    <input type="text" className="form-control" placeholder="date of birth" />
                                 </div>
                                 <div className="col-md-12 p-2">
                                     <label className="labels">Profile Picture</label>
-                                    <input type="text" className="form-control" placeholder="upload profile pic" value="" />
+                                    <input type="text" className="form-control" placeholder="upload profile pic" />
                                 </div>
                             </div>
                             <div className="mt-5 text-center">
