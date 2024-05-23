1eec6aad94fad1c403009f663d100f804950d89e
Rohit Kewat <rohitwebd@gmail.com>
2024-05-23 19:25:59 +0530

    testimolmalchfgfggfg

--- src/app/shared/testimonal.js
+++ src/app/shared/testimonal.js
@@ -1,104 +1,104 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function Testimonial () {
-    return (
-        <>
-        <div className="testimonial-section">
+export default function Testimonial() {
+  return (
+    <>
+      <div className="testimonial-section">
         <div className="container">
           <div className="row">
             <div className="col-lg-7 mx-auto text-center">
               <h2 className="section-title">Testimonials</h2>
             </div>
-          </div>
-
-          <div className="row justify-content-center">
-            <div className="col-lg-12">
-              <div className="testimonial-slider-wrap text-center">
-
-                <div id="testimonial-nav">
-                  <span className="prev" data-controls="prev"><FontAwesomeIcon icon={faChevronCircleLeft} /></span>
-                  <span className="next" data-controls="next"><FontAwesomeIcon icon={faChevronCircleRight} /></span>
-                </div>
-
-                <div className="testimonial-slider">
-
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                          </blockquote>
-
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                          </div>
-                        </div>
-
-                      </div>
-                    </div>
-                  </div>
-                  {/* <!-- END item --> */}
-
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                          </blockquote>
-
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                          </div>
-                        </div>
-
-                      </div>
-                    </div>
-                  </div>
-                  {/* <!-- END item --> */}
-
-                  <div className="item">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-8 mx-auto">
-
-                        <div className="testimonial-block text-center">
-                          <blockquote className="mb-5">
-                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                          </blockquote>
-
-                          <div className="author-info">
-                            <div className="author-pic">
-                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
-                            </div>
-                            <h3 className="font-weight-bold">Maria Jones</h3>
-                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                          </div>
-                        </div>
-
-                      </div>
-                    </div>
-                  </div>
-                  {/* <!-- END item --> */}
-
-                </div>
-
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
               </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div className="item">
+                    <div className="row justify-content-center">
+                      <div className="col-lg-8 mx-auto">
+
+                        <div className="testimonial-block text-center">
+                          <blockquote className="mb-5">
+                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
+                          </blockquote>
+
+                          <div className="author-info">
+                            <div className="author-pic">
+                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
+                            </div>
+                            <h3 className="font-weight-bold">Maria Jones</h3>
+                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div className="item">
+                    <div className="row justify-content-center">
+                      <div className="col-lg-8 mx-auto">
+
+                        <div className="testimonial-block text-center">
+                          <blockquote className="mb-5">
+                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
+                          </blockquote>
+
+                          <div className="author-info">
+                            <div className="author-pic">
+                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
+                            </div>
+                            <h3 className="font-weight-bold">Maria Jones</h3>
+                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div className="item">
+                    <div className="row justify-content-center">
+                      <div className="col-lg-8 mx-auto">
+
+                        <div className="testimonial-block text-center">
+                          <blockquote className="mb-5">
+                            <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
+                          </blockquote>
+
+                          <div className="author-info">
+                            <div className="author-pic">
+                              <img src="images/person-1.png" alt="Maria Jones" className="img-fluid" />
+                            </div>
+                            <h3 className="font-weight-bold">Maria Jones</h3>
+                            <span className="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
             </div>
           </div>
         </div>
       </div>
-        </>
-    )
+    </>
+  )
 }
